d8b5fb02fe5ed3c3ee223f5450f5fc84aea63388
zhangrifeng <zhangrifeng2000@163.com>
2025-01-12 16:16:54 +0800

    version 0.0.1

---
+++ src/planning_main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "process.h"
+using std::cout, std::endl;
+
+int main()
+{
+    cout <<"planing start!" <<endl;
+    Process pro;
+    pro.planProcess();
+    cout <<"planing end!" <<endl;
+    return 0;
+}
